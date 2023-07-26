8c323bcc4703c619e8f32acde9bdf3952efb3384
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-07-26 15:53:13 +0530

    A16

---
+++ BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace A16
+{
+    public class BankAccount
+    {
+        public Double AN;
+        public String Name;
+        public Double AB;
+
+        public BankAccount(Double AB)
+        {
+            this.AN = 55521;
+            this.Name = "Avneesh";
+            this.AB = 0;
+        }
+
+        public Double gAN { get { return AN; } }
+        public string gName { get { return Name; } }
+        public Double gAB { get { return AB; } } // Read-only property
+
+        public void Deposit(Double amount)
+        {
+            AB += amount;
+        }
+
+        public void Withdraw(Double amount)
+        {
+            if (AB >= amount)
+            {
+                AB -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds. Withdrawal failed.");
+            }
+        }
+
+        public void PrintAccountBalance(string passcode)
+        {
+            if (passcode == "Qwert")
+            {
+                Console.WriteLine("Your Balance is " + AB);
+            }
+            else
+            {
+                Console.WriteLine("Invalid passcode. Access denied.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace A16
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO Oyes Bank");
+
+            BankAccount Bk = new BankAccount(14);
+        start:
+
+            Console.WriteLine($"Account Number   :{Bk.gAN} \n" +
+                              $"Holder Name      :{Bk.gName}\n");
+
+            Console.WriteLine("Would you Like to Deposit New Balance,Press y for deposit\nPress m for withdraw amount\nWant to check bank Amount press b");
+
+            string y = Console.ReadLine();
+            switch (y)
+            {
+                case "y":
+                    Console.WriteLine("How much Amount You want to enter");
+                    double X = double.Parse(Console.ReadLine());
+                    Bk.Deposit(X);
+                    Console.WriteLine($"Your New Balance: {Bk.gAB}");
+                    break;
+                case "m":
+                    Console.WriteLine("How much Amount You want to Withdraw");
+                    double r = double.Parse(Console.ReadLine());
+                    Bk.Withdraw(r);
+                    Console.WriteLine($"Your New Balance: {Bk.gAB}");
+                    break;
+                case "b":
+                    Console.WriteLine("ENTER LOGIN PASSWORD");
+                    string passcode = Console.ReadLine();
+                    Bk.PrintAccountBalance(passcode);
+                    break;
+                default:
+                    Console.WriteLine("Invalid key");
+                    goto start;
+            }
+            Console.WriteLine("Would You Like to Continue? Press 's' to continue");
+            string s = Console.ReadLine();
+            if (s == "s")
+            {
+                goto start;
+            }
+        }
+    }
+}
